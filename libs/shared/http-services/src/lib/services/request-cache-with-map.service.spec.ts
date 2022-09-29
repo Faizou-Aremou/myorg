02059c1e27688900e5da37f9cba7f4412fbb5907
Faizou <faizouaremou@gmail.com>
2022-09-29 15:34:34 +0200

    add test workflow to request cache service

--- libs/shared/http-services/src/lib/services/request-cache-with-map.service.spec.ts
+++ libs/shared/http-services/src/lib/services/request-cache-with-map.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { CACHEABLE_URLS } from '../tokens/cacheable-urls';
+import { MAX_AGE } from '../tokens/max-age';
 
 import { RequestCacheWithMapService } from './request-cache-with-map.service';
 
@@ -6,11 +8,22 @@ describe('RequestCacheWithMapService', () => {
   let service: RequestCacheWithMapService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: CACHEABLE_URLS, useValue: ['/test'] },
+        { provide: MAX_AGE, useValue: 3000 },
+      ],
+    });
     service = TestBed.inject(RequestCacheWithMapService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  // it('should get cached response', () => {
+  //   expect(service).toBeTruthy();
+  // });
+  // it('should put response in cache', () => {
+  //   expect(service).toBeTruthy();
+  // });
 });
