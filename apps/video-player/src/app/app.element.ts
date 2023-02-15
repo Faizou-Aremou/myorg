683893dae22d31b07a236fc780383ac4f6988663
Faizou <faizouaremou@gmail.com>
2023-02-15 11:08:15 +0100

    improve web component

--- apps/video-player/src/app/app.element.ts
+++ apps/video-player/src/app/app.element.ts
@@ -1,17 +1,62 @@
 import style from '!!raw-loader!sass-loader!./app.element.scss';
+import { isEmpty, head, tail } from 'ramda';
 import template from 'raw-loader!./app.element.html';
 
 const styleElement = document.createElement('style');
-styleElement.innerHTML =  style;
+styleElement.innerHTML = style;
 
 export class AppElement extends HTMLElement {
+  // AppElement is "host"
   public static observedAttributes = [];
 
   connectedCallback() {
     const title = 'video-player';
+    this.attachComponentTemplateToHost(
+      template,
+      this.createBindedProperties([['${title}', title]])
+    );
+  }
+
+  attachComponentTemplateToHost(
+    template: string,
+    properties: BindedProperties
+  ): void {
     this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = template.replace('${title}', title);
+    const bindedTemplate = this.bindingHostPropertiesToComponentVariables(
+      properties,
+      template
+    );
+    this.shadowRoot.innerHTML = bindedTemplate;
+    this.addStyleToShadowRoot(styleElement);
+  }
+  addStyleToShadowRoot(styleElement: HTMLStyleElement): void {
     this.shadowRoot.appendChild(styleElement);
+  }
+  createBindedProperties(
+    bindedProperties: [string, unknown][]
+  ): BindedProperties {
+    return bindedProperties as BindedProperties;
+  }
+  private bindingHostPropertiesToComponentVariables(
+    properties: BindedProperties,
+    template: string
+  ): string {
+    return replacePropertiesValuesInTemplate(template, properties);
   }
 }
 customElements.define('myorg-root', AppElement);
+
+export type BindedProperties = [string, unknown][];
+function replacePropertiesValuesInTemplate(
+  template: string,
+  properties: [string, unknown][]
+): string {
+  if (isEmpty(properties)) {
+    return template;
+  }
+  const headElement = head(properties);
+  return replacePropertiesValuesInTemplate(
+    template.replace(headElement[0], `${headElement[1]}`),
+    tail(properties)
+  );
+}
