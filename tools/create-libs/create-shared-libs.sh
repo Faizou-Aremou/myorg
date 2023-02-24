fcb1c546368ed645127f08964b3abc06b5d0a5f1
Faizou <faizouaremou@gmail.com>
2023-02-24 14:40:44 +0100

    add shared lib scripts

---
+++ tools/create-libs/create-shared-libs.sh
@@ -0,0 +1,18 @@
+
+# animation
+npx nx g @nrwl/node:lib util-animation --directory shared --buildable --publishable --importPath @web-times-team/util-animation
+
+# figure
+npx nx g @nrwl/node:lib util-figure --directory shared --buildable --publishable --importPath @web-times-team/util-figure
+
+# ui
+npx nx g @nrwl/node:lib util-ui --directory shared --buildable --publishable --importPath @web-times-team/util-ui
+
+# media
+npx nx g @nrwl/node:lib util-media --directory shared --buildable --publishable --importPath @web-times-team/util-media
+
+# canvas
+npx nx g @nrwl/node:lib util-canvas --directory shared --buildable --publishable --importPath @web-times-team/util-canvas
+
+# web-components
+npx nx g @nrwl/js:lib util-web-components --directory shared --buildable --publishable --importPath @web-times-team/util-web-components
