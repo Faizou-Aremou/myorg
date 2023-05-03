4c58aa7072ccfdb478996555224515c1a59d62ed
Faizou <faizouaremou@gmail.com>
2023-05-03 08:14:22 +0200

    install forms-files project

--- tools/create-apps/create-apps.sh
+++ tools/create-apps/create-apps.sh
@@ -1,2 +1,8 @@
 # device orientation
 npx nx g @nrwl/web:app device-orientation 
+
+# forms-and-files
+npx nx g @nrwl/angular:app forms-and-files
+
+# forms-and-files-api
+npx nx g @nrwl/express:app --frontendProject=forms-and-files
--- tools/create-apps/delete-apps.sh
+++ tools/create-apps/delete-apps.sh
@@ -1,2 +1,6 @@
 # orientation device
 npx nx g @nrwl/workspace:remove device-orientation
+# forms-and-files
+npx nx g @nrwl/workspace:remove forms-and-files
+# forms-and-files-api
+npx nx g @nrwl/workspace:remove forms-and-files-api
